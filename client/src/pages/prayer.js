7cad6d7aaa736cc57b7c0e7e51f44d512c7e8ef8
bouthaina5 <bouthainabenhamida@gmail.com>
2022-09-10 10:46:37 +0200

    update 3

--- client/src/pages/prayer.js
+++ client/src/pages/prayer.js
@@ -1,6 +1,4 @@
 import {useContext} from 'react';
-// import { Link } from 'react-router-dom'
-
 import Clock from '../components/Clock'
 import HijriDate from '../components/HijriDate'
 import PrayTimes from '../components/PrayTimes'
